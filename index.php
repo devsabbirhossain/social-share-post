703fcd813e2b447049563461ac0bd70f9dd47bf5
Md Sabbir Hossain Shawon <sabbir2dev@gmail.com>
2022-12-30 20:24:06 +0600

    update funtions and security

--- index.php
+++ index.php
@@ -1,3 +1,147 @@
 <?php
 
-//Silence Is Good
+
+/*
+ * Define your namespaces here by use keyword
+ * */
+
+use Plugin_Name_Name_Space\Includes\Init\{
+ Constant, Activator
+};
+use Plugin_Name_Name_Space\Includes\Config\Initial_Value;
+use Plugin_Name_Name_Space\Includes\Uninstall\{
+	Deactivator, Uninstall
+};
+
+/**
+ * If this file is called directly, then abort execution.
+ */
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+
+
+final class Plugin_Name_Plugin {
+	/**
+	 * Instance property of Plugin_Name_Plugin Class.
+	 * This is a property in your plugin primary class. You will use to create
+	 * one object from Plugin_Name_Plugin class in whole of program execution.
+	 *
+	 * @access private
+	 * @var    Plugin_Name_Plugin $instance create only one instance from plugin primary class
+	 * @static
+	 */
+	private static $instance;
+	/**
+	 * @var Initial_Value $initial_values An object  to keep all of initial values for theme
+	 */
+	protected $initial_values;
+
+
+	/**
+	 * Plugin_Name_Plugin constructor.
+	 * It defines related constant, include autoloader class, register activation hook,
+	 * deactivation hook and uninstall hook and call Core class to run dependencies for plugin
+	 *
+	 * @access private
+	 */
+	public function __construct() {
+		/*Define Autoloader class for plugin*/
+		$autoloader_path = 'includes/class-autoloader.php';
+		/**
+		 * Include autoloader class to load all of classes inside this plugin
+		 */
+		require_once trailingslashit( plugin_dir_path( __FILE__ ) ) . $autoloader_path;
+		/*Define required constant for plugin*/
+		Constant::define_constant();
+
+		/**
+		 * Register activation hook.
+		 * Register activation hook for this plugin by invoking activate
+		 * in Plugin_Name_Plugin class.
+		 *
+		 * @param string   $file     path to the plugin file.
+		 * @param callback $function The function to be run when the plugin is activated.
+		 */
+		register_activation_hook(
+			__FILE__,
+			function () {
+				$this->activate(
+					new Activator( intval( get_option( 'last_your_plugin_name_dbs_version' ) ) )
+				);
+			}
+		);
+		/**
+		 * Register deactivation hook.
+		 * Register deactivation hook for this plugin by invoking deactivate
+		 * in Plugin_Name_Plugin class.
+		 *
+		 * @param string   $file     path to the plugin file.
+		 * @param callback $function The function to be run when the plugin is deactivated.
+		 */
+		register_deactivation_hook(
+			__FILE__,
+			array( $this, 'deactivate' )
+		);
+	}
+
+	/**
+	 * Call activate method.
+	 * This function calls activate method from Activator class.
+	 * You can use from this method to run every thing you need when plugin is activated.
+	 *
+	 * @access public
+	 * @since  1.0.0
+	 * @see    Plugin_Name_Name_Space\Includes\Init\Activator Class
+	 */
+	public function activate( Activator $activator_object ) {
+		global $wpdb;
+		$activator_object->activate(
+			true,
+			new Table( $wpdb, PLUGIN_NAME_DB_VERSION, get_option( 'has_table_name' ) )
+		);
+	}
+
+	/**
+	 * Create an instance from Plugin_Name_Plugin class.
+	 *
+	 * @access public
+	 * @since  1.0.0
+	 * @return Plugin_Name_Plugin
+	 */
+	public static function instance() {
+		if ( is_null( ( self::$instance ) ) ) {
+			self::$instance = new self();
+		}
+
+		return self::$instance;
+	}
+	
+
+	/**
+	 * Load Core plugin class.
+	 *
+	 * @access public
+	 * @since  1.0.0
+	 */
+	public function run_plugin_name_plugin() {
+		// TODO: Do you codes here to run the plugin
+	}
+
+	/**
+	 * Call deactivate method.
+	 * This function calls deactivate method from Dectivator class.
+	 * You can use from this method to run every thing you need when plugin is deactivated.
+	 *
+	 * @access public
+	 * @since  1.0.0
+	 */
+	public function deactivate() {
+		Deactivator::deactivate();
+	}
+}
+
+
+$plugin_name_plugin_object = Plugin_Name_Plugin::instance();
+$plugin_name_plugin_object->run_plugin_name_plugin();
--- social-share.php
+++ social-share.php
@@ -1,13 +1,16 @@
 <?php
 
 /*
-* Plugin Name: Social Share Post
-* Plugin URI: https://brothersdeveloper.com
-* Description: This Plugin is basically for share your post and blogs to social site like facebook, linkdin, twitter etc.
-* Author: Sabbir Hossain
-* Author URI: https://github.com/devsabbirhossain
-* Version: 1.0.0
-* Text Domain: ssp
+* @wordpress-plugin
+* Plugin Name: 	Social Share Post
+* Plugin URI: 	https://github.com/devsabbirhossain/social-share-post
+* Description: 	This Plugin is basically for share your post and blogs to social site like facebook, linkdin, twitter etc.
+* Author: 		Md Sabbir Hossain Shawon <sabbir2dev@gmail.com>
+* Author URI: 	https://github.com/devsabbirhossain
+* Version: 		1.0.0
+* Text Domain: 	ssp
+* License:		GPL-2.0+
+* License URI:  http://www.gnu.org/licenses/gpl-2.0.txt
 */
 
 if ( ! defined( 'ABSPATH' ) )
@@ -15,10 +18,16 @@ if ( ! defined( 'ABSPATH' ) )
     exit; // Exit if accessed directly
 }
 
-
 define('PLUGIN_URL', plugin_dir_path( __FILE__ ));
 define('PLUGIN_URL_INCLUDE', PLUGIN_URL.'/inc');
 define('PLUGIN_URL_ASSETS', PLUGIN_URL.'/assets');
+
+function ssp_load_plugin(){
+    $plugin_dir = basename(dirname(__FILE__))."/languages/";
+    load_plugin_textdomain( 'ssp', false, $plugin_dir );
+}
+add_action( 'plugins_loaded', 'ssp_load_plugin' );
+
 /**
  * add file and functinos for this plugin
  */
@@ -27,9 +36,29 @@ class SocialShare
 	
 	function __construct()
 	{
+		register_activation_hook( __FILE__, array( $this, 'activation_hook' ) );
+		register_deactivation_hook( __FILE__, array( $this, 'deactivation_hook' ) );
 		add_action( 'wp_enqueue_scripts',array( $this, 'add_scripts_frontend' ) );
 		add_action( 'admin_enqueue_scripts', array( $this,'add_scripts_admin_page' ) );
 		$this->add_other_files();
+	}
+
+	//this function will work when this plugin will activate
+	public function activation_hook(){
+		update_option( 'facebook-ssp', true );
+        update_option( 'linkdin-ssp', true );
+        delete_option( 'beforeContent-ssp', true );
+	}
+
+	//this function will work when this plugin will deactivate
+	public function deactivation_hook(){
+		delete_option( 'facebook-ssp' );
+        delete_option( 'twitter-ssp' );
+        delete_option( 'linkdin-ssp' );
+        delete_option( 'digg-ssp' );
+        delete_option( 'pinterest-ssp' );
+        delete_option( 'afterContent-ssp' );
+        delete_option( 'beforeContent-ssp' );
 	}
 
 	public function add_other_files(){
